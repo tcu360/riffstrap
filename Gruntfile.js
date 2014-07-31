098f4b0fd1ab6962555cf5d32719a20d96da75ac
Andrew Chavez <adchavez@gmail.com>
2014-07-31 15:16:01 -0500

    Drop unused concat task

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
